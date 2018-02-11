fefde3ff3e33003058ddb24902860b122ac9a95c
Andrew Brown <andrew.brown5717@gmail.com>
2018-02-11 02:05:01 -0600

    add beginnings of single calendar day

---
+++ src/components/Day/Day.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Day.css';
+
+class Day extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div onClick={() => this.props.onDayClick()} className="day">
+                <div className="day__date">
+                    {this.props.date}
+                </div>
+                <ul>
+                    {
+                        this.props.events.map((event) => {
+                            return (
+                                <li
+                                    onClick={() => this.props.onEventClick(event.id)}
+                                    className="day__event">
+                                        {event.title}
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+        );
+
+    }
+
+}
